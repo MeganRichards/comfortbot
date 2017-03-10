d955ecf672f4b1b4ab72af4a0ebb6c58deac7001
patsobo <psobolew@mymail.mines.edu>
2017-03-09 20:27:05 -0700

    Implemented some more server methods for creating fake data.

--- server/method.js
+++ server/method.js
@@ -6,10 +6,33 @@ Meteor.methods({
 	 */
 	'insertRoom': function(room) {
 		Room.insert({
-			name: post.name,
-			dim: post.dim,
-			points: post.points
+			name: room.name,
+			dim: room.dim,
+			points: room.points
 			created_at: new Date(),
 		});
 	},
+	
+	/**
+	 * Creates a new Map in the database with the specified attributes.
+	 *
+	 * @param map The map to be created
+	 */
+	'insertMap': function(map) {
+		Maps.insert({
+			loc: map.loc,
+			temp: map.temp,
+			rad_temp: map.radtemp,
+			humidity: map.humid,
+			velocity: map.velocity
+		});
+	},
+
+	/**
+	 * Deletes all the data maps in the database.
+	 *
+	 */
+	'deleteMaps': function() {
+		Maps.remove({});
+	}
 });
