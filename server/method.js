9c13dbf7e5da13210a7cc3313feba4eb6ffce398
patsobo <psobolew@mymail.mines.edu>
2017-03-09 22:40:51 -0700

    Added babel dependency and a text file with a sample database.

--- server/method.js
+++ server/method.js
@@ -5,34 +5,48 @@ Meteor.methods({
 	 * @param room The room to be created
 	 */
 	'insertRoom': function(room) {
-		Room.insert({
+		Rooms.insert({
 			name: room.name,
 			dim: room.dim,
-			points: room.points
+			points: room.points,
+			created_at: new Date(),
+		});
+	},
+
+	/**
+	 * Creates a new Map in the database with the specified attributes.
+	 *
+	 * @param room The room to be created
+	 */
+	'insertMap': function(map) {
+		Maps.insert({
+			name: map.name,
+			dim: map.dim,
+			points: map.points,
 			created_at: new Date(),
 		});
 	},
 	
 	/**
-	 * Creates a new Map in the database with the specified attributes.
+	 * Creates a new data Point in the database with the specified attributes.
 	 *
-	 * @param map The map to be created
+	 * @param point The point to be created
 	 */
-	'insertMap': function(map) {
-		Maps.insert({
-			loc: map.loc,
-			temp: map.temp,
-			rad_temp: map.radtemp,
-			humidity: map.humid,
-			velocity: map.velocity
+	'insertData': function(point) {
+		Points.insert({
+			loc: point.loc,
+			temp: point.temp,
+			rad_temp: point.radtemp,
+			humidity: point.humid,
+			velocity: point.velocity
 		});
 	},
 
 	/**
-	 * Deletes all the data maps in the database.
+	 * Deletes all the data points in the database.
 	 *
 	 */
-	'deleteMaps': function() {
-		Maps.remove({});
+	'deleteData': function() {
+		Points.remove({});
 	}
 });
