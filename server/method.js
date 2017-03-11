e3fb06fc06a580c78def670ab06ae2e08abe31c2
patsobo <psobolew@mymail.mines.edu>
2017-03-11 10:03:44 -0700

    Implemented chunk of database with some errors still.

--- server/method.js
+++ server/method.js
@@ -14,39 +14,42 @@ Meteor.methods({
 	},
 
 	/**
-	 * Creates a new Map in the database with the specified attributes.
-	 *
-	 * @param room The room to be created
-	 */
-	'insertMap': function(map) {
-		Maps.insert({
-			name: map.name,
-			dim: map.dim,
-			points: map.points,
-			created_at: new Date(),
-		});
-	},
-	
-	/**
 	 * Creates a new data Point in the database with the specified attributes.
 	 *
+	 * @param room_name The name of the room to attach the point to
 	 * @param point The point to be created
 	 */
-	'insertData': function(point) {
-		Points.insert({
-			loc: point.loc,
-			temp: point.temp,
-			rad_temp: point.radtemp,
-			humidity: point.humid,
-			velocity: point.velocity
-		});
+	'insertData': function(room_name, point) {
+		d = new Date();
+		date_key = "" + d.getFullYear() + d.getMonth() + d.getDate();
+		key = "x" + point.x + "y" + point.y;
+		
+		entry = {
+    		"temp" : point.temp,
+    		"rad_temp" : point.radtemp,
+    		"humidity" : point.humid,
+    		"velocity" : point.velocity
+		};
+
+//		date = {};
+//    	date[key] = {};
+//    	date.key["temp"] = point.temp;
+//    	date.key["rad_temp"] = point.radtemp;
+//    	date.key["humidity"] = point.humid;
+//    	date.key["velocity"] = point.velocity;
+//		
+//		Rooms.upsert(name: room_name, {
+//			$push: {
+//				date_key.key: entry
+//			}
+//		});
 	},
 
 	/**
-	 * Deletes all the data points in the database.
+	 * Deletes all the rooms in the database.
 	 *
 	 */
 	'deleteData': function() {
-		Points.remove({});
+		Rooms.remove({});
 	}
 });
