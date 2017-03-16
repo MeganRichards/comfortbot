60e07612b0f483a3d0ea0b4d37749ea300384d0e
Patrick Sobolewski <psobolew@mymail.mines.edu>
2017-03-16 08:36:40 -0600

    Messed around with d3 and thought I pushed it but ok.

--- server/main.js
+++ server/main.js
@@ -1,12 +1,21 @@
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(function () {
-	if (Circles.find().count() === 0) {
-  		Circles.insert({data: [5, 8, 11, 14, 17, 20]});
-	}
+  var date = new Date();
+  var map = {
+    room: "BBW280",
+    year: date.getFullYear(),
+    month: date.getMonth(),
+    day: date.getDate()
+  };
+  Meteor.call('updateLoadedMap', map);
+  
+  if (Circles.find().count() === 0) {
+    Circles.insert({data: [5, 8, 11, 14, 17, 20]});
+  }
 });
 
-Meteor.setInterval(function () {
-	var newData = _.shuffle(Circles.findOne().data);
-	Circles.update({}, {data: newData});
-}, 2000);
+// Meteor.setInterval(function () {
+// 	var newData = _.shuffle(Circles.findOne().data);
+// 	Circles.update({}, {data: newData});
+// }, 2000);
